8eceb3a01311ebed31432188cef6eacaf1235b7b
Ayooluwa <olusegunstephen222@gmail.com>
2026-01-06 07:57:35 +0100

    Fix: Remove .next and ignore build directory

--- .next/dev/static/chunks/3f47e_src_app_(admin)_(Students)_students_allstudents_page_tsx_d322aa19._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/School Management System_220a9ca3._.js",
-  "static/chunks/290b9_fb570a05._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/bc8bc_hool Management System_src_app_(admin)_(teachers)_teachers_add_page_tsx_d322aa19._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/290b9_88132214._.js",
-  "static/chunks/School Management System_89d24033._.js"
-],
-    source: "dynamic"
-});
