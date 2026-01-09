d4f2f5f2122cfda5fbc74920fd557be13494122c
Ayooluwa <olusegunstephen222@gmail.com>
2026-01-09 13:35:32 +0100

    Bug/Deleted Next js cached files

--- .next/dev/static/chunks/3f47e_src_app_(admin)_(Students)_students_profile_[id]_page_tsx_d322aa19._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/School Management System_98d8dd75._.js",
-  "static/chunks/290b9_385662f8._.js"
-],
-    source: "dynamic"
-});
